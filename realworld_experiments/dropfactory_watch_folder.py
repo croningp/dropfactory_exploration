a7a9403a93d46b57e2190301ea7395dbfb80e315
Jonathan Grizou <jonathan.grizou@glasgow.ac.uk>
2016-07-12 18:23:02 +0100

    debug in file, default purge at 4, clean_oil_head when exiting

--- realworld_experiments/dropfactory_watch_folder.py
+++ realworld_experiments/dropfactory_watch_folder.py
@@ -13,7 +13,8 @@ dropfactory_path = os.path.join(HERE_PATH, '..', '..', 'dropfactory', 'software'
 sys.path.append(dropfactory_path)
 
 import logging
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='dropfactory.log', filemode='w', level=logging.DEBUG)
 
 #
 import time
@@ -33,6 +34,7 @@ def send_email_notification(subject, body):
     for toaddr in EMAILS_TO_NOTIFY:
         email_notification.send_email_notification(toaddr, subject, body)
 
+
 def end_of_day_sequence(watcher):
     print 'Automatically stopping dropfactory at {}'.format(time.ctime())
     send_email_notification('[Dropfactory] Shuting down', 'Automatically stopping dropfactory at {}'.format(time.ctime()))
@@ -41,6 +43,8 @@ def end_of_day_sequence(watcher):
     manager.bypass_waste_security = True  # this should only be done in extreme case like this one
     print 'Waiting for all ongoing XP to finish...'
     manager.wait_until_XP_finished()
+    print 'Cleaning oil head'
+    manager.clean_oil_head()
     print 'Shutting down python...'
     os._exit(0)
 
@@ -54,10 +58,23 @@ if __name__ == '__main__':
 
     pool_folder = os.path.join(HERE_PATH, sys.argv[1])
 
-    response = raw_input('## Do you want to quickly prime the oils and surfactant [y/N]: ')
-    if response in ['y', 'Y']:
-        manager.add_purge_sequence_XP(n_purge=2)
+    # asking for purge
+    response = raw_input('## Do you want to quickly prime the oils and surfactant [Y/n]: ')
+    if response not in ['n', 'N']:
+        user_input_validated = False
+        while not user_input_validated:
+            response = raw_input('How many purge do you want [default is 4]: ')
+            if response == '':
+                response = '4'
+            if response.isdigit():
+                n_purge = int(response)
+                user_input_validated = True
+            else:
+                print '{} is not a valid number, you must provide a positive int or 0'.format(response)
+        print 'Great, purging {} times'.format(n_purge)
+        manager.add_purge_sequence_XP(n_purge=n_purge)
 
+    # start to add XP watched to manager
     watcher = XPWatcher(manager, pool_folder)
 
     time_keeping = AboveHourEvent(LAST_HOUR, end_of_day_sequence, watcher)
