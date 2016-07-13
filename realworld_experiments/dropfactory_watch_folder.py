5660cff86c1ce94cc7c917aa9e86233fff997d6b
Jonathan Grizou <jonathan.grizou@glasgow.ac.uk>
2016-07-13 11:53:23 +0100

    added clean surfactant pump to end_of_day_sequence

--- realworld_experiments/dropfactory_watch_folder.py
+++ realworld_experiments/dropfactory_watch_folder.py
@@ -43,8 +43,10 @@ def end_of_day_sequence(watcher):
     manager.bypass_waste_security = True  # this should only be done in extreme case like this one
     print 'Waiting for all ongoing XP to finish...'
     manager.wait_until_XP_finished()
-    print 'Cleaning oil head'
+    print 'Cleaning oil head...'
     manager.clean_oil_head()
+    print 'Cleaning surfactant pump...'
+    manager.clean_surfactant_pump()
     print 'Shutting down python...'
     os._exit(0)
 
