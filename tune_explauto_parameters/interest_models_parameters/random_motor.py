849711318743acd064525922cf4b38724d9b8619
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-08-01 15:26:56 +0100

    inters tuning scripts

---
+++ tune_explauto_parameters/interest_models_parameters/random_motor.py
@@ -0,0 +1,14 @@
+import os
+
+# this get our current location in the file system
+import inspect
+HERE_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+
+import tools
+
+
+if __name__ == '__main__':
+
+    results = tools.run_random_motor()
+    tools.save_to_json(results, os.path.join(HERE_PATH, 'random_motor.json'))
